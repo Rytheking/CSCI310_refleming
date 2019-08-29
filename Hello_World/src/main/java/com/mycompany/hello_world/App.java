97c5288eb3e02ee0637c7e89b84d1415627966eb
Ryan Fleming <ryanelliottfleming@gmail.com>
2019-08-29 11:14:25 -0600

    Build File

---
+++ Hello_World/src/main/java/com/mycompany/hello_world/App.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.hello_world;
+
+/**
+ *
+ * @author ryanfleming
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*contructing an app*/
+        App app = new App();
+        app.run();
+    }
+
+    private void run() {
+        System.out.println("Hello Boy, you're looking dapper today");
+    }
+}
