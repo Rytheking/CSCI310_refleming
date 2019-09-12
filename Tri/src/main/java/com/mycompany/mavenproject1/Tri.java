299858da713077a9440163c683276de529809355
Ryan Fleming <ryanelliottfleming@gmail.com>
2019-09-12 11:12:00 -0600

    Working syntax, will subit to kattis

--- Tri/src/main/java/com/mycompany/mavenproject1/Tri.java
+++ Tri/src/main/java/com/mycompany/mavenproject1/Tri.java
@@ -5,6 +5,8 @@
  */
 package com.mycompany.mavenproject1;
 
+import java.util.*;
+        
 /**
  *
  * @author ryanfleming
@@ -15,66 +17,77 @@ public class Tri {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int a = 5;
-        int b = 3;
-        int c = 8;
+    
+        
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLong()) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
         algorithm(a,b,c);
         
-        a= 5;
-        b=15;
-        c=3;
-        algorithm(a,b,c);
-        
+        }
     }
     
     public static void algorithm(int a,int b,int c)
     {
-        testAddition(a,b,c);
-        testSubtraction(a,b,c);
-        testMultiplication(a,b,c);
-        testDivision(a,b,c);
+        if(testAddition(a,b,c)==1) return;
+        else if(testSubtraction(a,b,c)==1) return;
+        else if(testMultiplication(a,b,c)==1) return;
+        else if(testDivision(a,b,c)==1) return;
     }
     
-    public static void testAddition(int a,int b,int c)
+    public static int testAddition(int a,int b,int c)
     {
         if(a+b==c){
-            System.out.println( a + " + " + b + " = " + c);
+            System.out.println( a + "+" + b + "=" + c);
+            return 1;
         }
         else if(a==b+c){
-            System.out.println(a + " = " + b + " + " + c);
+            System.out.println(a + "=" + b + "+" + c);
+            return 1;
         }
+        return 0;
         
     }
     
-    public static void testSubtraction(int a,int b,int c)
+    public static int testSubtraction(int a,int b,int c)
     {
         if(a-b==c){
-            System.out.println( a + " - " + b + " = " + c);
+            System.out.println( a + "-" + b + "=" + c);
+            return 1;
         }
         if(a==b-c){
-            System.out.println( a + " = " + b + " - " + c);
+            System.out.println( a + "=" + b + "-" + c);
+            return 1;
         }
+        return 0;
     }
     
-    public static void testMultiplication(int a,int b,int c)
+    public static int testMultiplication(int a,int b,int c)
     {
         if(a*b==c){
-            System.out.println( a + " * " + b + " = " + c);
+            System.out.println( a + "*" + b + "=" + c);
+            return 1;
         }
         if(a==b*c){
-            System.out.println( a + " = " + b + " * " + c);
+            System.out.println( a + "=" + b + "*" + c);
+            return 1;
         }
+        return 0;
     }
     
-    public static void testDivision(int a,int b,int c)
+    public static int testDivision(int a,int b,int c)
     {
         if(a/b==c){
-            System.out.println( a + " / " + b + " = " + c);
+            System.out.println( a + "/" + b + " = " + c);
+            return 1;
         }
         if(a==b/c){
-            System.out.println( a + " = " + b + " / " + c);
+            System.out.println( a + "=" + b + "/" + c);
+            return 1;
         }
-        
+        return 0;
     }
     
 }
