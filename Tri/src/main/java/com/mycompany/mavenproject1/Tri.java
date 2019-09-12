cf3e104e99665209a081c9070af6f366211c6a5c
Ryan Fleming <ryanelliottfleming@gmail.com>
2019-09-12 09:23:59 -0600

    Skeleton program

--- Tri/src/main/java/com/mycompany/mavenproject1/Tri.java
+++ Tri/src/main/java/com/mycompany/mavenproject1/Tri.java
@@ -15,7 +15,43 @@ public class Tri {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int a = 5;
+        int b = 3;
+        int c = 8;
+        
+    }
+    
+    public String algorithm(int a,int b,int c)
+    {
+        testAddition(a,b,c);
+        testSubtraction(a,b,c);
+        testMultiplication(a,b,c);
+        testDivision(a,b,c);
+        return "yellow";
+    }
+    
+    public String testAddition(int a,int b,int c)
+    {
+        
+        return "yellow";
+    }
+    
+    public String testSubtraction(int a,int b,int c)
+    {
+        
+        return "yellow";
+    }
+    
+    public String testMultiplication(int a,int b,int c)
+    {
+        
+        return "yellow";
+    }
+    
+    public String testDivision(int a,int b,int c)
+    {
+        
+        return "yellow";
     }
     
 }
