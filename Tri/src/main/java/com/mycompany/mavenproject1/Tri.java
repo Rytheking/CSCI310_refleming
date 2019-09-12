63e57a2e319a3fbc07283fde1e2372c586ee02cc
Ryan Fleming <ryanelliottfleming@gmail.com>
2019-09-12 09:37:18 -0600

    Working on algorithm

--- Tri/src/main/java/com/mycompany/mavenproject1/Tri.java
+++ Tri/src/main/java/com/mycompany/mavenproject1/Tri.java
@@ -18,38 +18,89 @@ public class Tri {
         int a = 5;
         int b = 3;
         int c = 8;
-        
+        algorithm(a,b,c);
     }
     
-    public String algorithm(int a,int b,int c)
+    public static void algorithm(int a,int b,int c)
     {
         testAddition(a,b,c);
         testSubtraction(a,b,c);
         testMultiplication(a,b,c);
         testDivision(a,b,c);
+    }
+    
+    public static void testAddition(int a,int b,int c)
+    {
+        if(a+b==c){
+            System.out.println( a + " + " + b + " = " + c);
+        }
+        else if(a+c==b){
+            System.out.println(a + " + " + c + " = " + b);
+        }
+        else if(c+b==a){
+           System.out.println( c + " + " + b + " = " + a);
+        }
+        
+    }
+    
+    public static String testSubtraction(int a,int b,int c)
+    {
+        if(a-b==c){
+            return a + " - " + b + " = " + c;
+        }
+        if(a-c==b){
+            return a + " - " + c + " = " + b;
+        }
+        if(b-a==c){
+            return b + " - " + a + " = " + c;
+        }
+        if(b-c==a){
+            return b + " - " + c + " = " + a;
+        }
+        if(c-a==b){
+            return c + " - " + a + " = " + b;
+        }
+        if(c-b==a){
+            return c + " - " + b + " = " + a;
+        }
         return "yellow";
     }
     
-    public String testAddition(int a,int b,int c)
+    public static String testMultiplication(int a,int b,int c)
     {
+        if(a*b==c){
+            
+        }
+        if(a*c==b){
+            
+        }
+        if(c*b==a){
+            
+        }
         
         return "yellow";
     }
     
-    public String testSubtraction(int a,int b,int c)
+    public static String testDivision(int a,int b,int c)
     {
-        
-        return "yellow";
-    }
-    
-    public String testMultiplication(int a,int b,int c)
-    {
-        
-        return "yellow";
-    }
-    
-    public String testDivision(int a,int b,int c)
-    {
+        if(a/b==c){
+            
+        }
+        if(a/c==b){
+            
+        }
+        if(b/a==c){
+            
+        }
+        if(b/c==a){
+            
+        }
+        if(c/a==b){
+            
+        }
+        if(c/b==a){
+            
+        }
         
         return "yellow";
     }
