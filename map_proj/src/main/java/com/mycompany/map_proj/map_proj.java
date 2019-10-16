8b72f8ffbe6d47263f6012157e386ecb30a77855
Ryan Fleming <ryanelliottfleming@gmail.com>
2019-10-16 14:04:53 -0600

    Clean up collection

--- map_proj/src/main/java/com/mycompany/map_proj/map_proj.java
+++ map_proj/src/main/java/com/mycompany/map_proj/map_proj.java
@@ -31,47 +31,44 @@ public class map_proj {
     /**
      * @param args the command line arguments
      */
-    
-    public static void main(String[] args) {
-        HashMap<String,Contactcard> contactBook=new HashMap<String,Contactcard>();
-        System.out.println("Welcome to the Contact Book! When adding cards, you'll be asked for name, number, and email. \n The only one required is the name, leave the others blank if you need");
-        System.out.println("Would you like to enter contact cards? y/n");
-        boolean cont =true;
+    public static HashMap<String,Contactcard> contactBook=new HashMap<String,Contactcard>();
+    public static void printThingsMan(){
+        System.out.println("What contact would you like to print? (if none just hit enter)");
         Scanner myObj = new Scanner(System.in);
         String response = myObj.nextLine();
-        if(response.equals("y"))
-        {
-        int i=1;
-        while(cont){
+        if(contactBook.containsKey(response)){
+            contactBook.get(response).printCard();
+            printThingsMan();
+        }
+        else{
+        System.out.println("We couldn't find that contact. Try again? y/n");
+            String r1= myObj.nextLine();
+            if(r1.equals("y"))
+               printThingsMan();
+        }
+    }
+    public static void main(String[] args) {
+       
+        System.out.println("Welcome to the Contact Book! When adding cards, you'll be asked for name, number, and email. \n The only one required is the name, leave the others blank if you need");
+        System.out.println("How many contact cards would you like to enter?");
+        boolean cont =true;
+        Scanner myObj = new Scanner(System.in);
+        int max = myObj.nextInt();
+        myObj.nextLine();
+        for(int i=1; i<=max; i++){
             System.out.println("Please input contact " + i +" name");
             String name=myObj.nextLine();
             System.out.println("Please input contact " + i +" phone");
-            
             int phonenum=myObj.nextInt();
+            myObj.nextLine();
             
             contactBook.put(name, new Contactcard(name, phonenum));
-           
-            System.out.println("Would you like to continue? y/n");
-            
-            String r2 = myObj.nextLine();
-            if(!r2.equals("y"))
-            {
-                cont=false;
-            }
         }
-        System.out.println("What contact would you like to print?");
-        response = myObj.nextLine();
-        if(contactBook.containsKey(response)){
-            contactBook.get(response).printCard();
-        }
-        else{
-            System.out.println("We couldn't find that contact");
-        }
+           if(max!=0)
+            printThingsMan();
     }
-        else{
-            System.out.println("Why'd you open this then?");
-        }
         
    }
     
-}
+    
+
