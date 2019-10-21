439e0b377bda69a8fb35ded0e6246ebf6e18d4e2
Ryan Fleming <ryanelliottfleming@gmail.com>
2019-10-21 12:49:24 -0600

    Implement basics of threading for for loops

--- threading_proj/src/main/java/com/mycompany/threading_proj/threading.java
+++ threading_proj/src/main/java/com/mycompany/threading_proj/threading.java
@@ -4,18 +4,35 @@
  * and open the template in the editor.
  */
 package com.mycompany.threading_proj;
-
+import java.util.Scanner;
 /**
  *
  * @author ryanfleming
  */
+class Thread2 extends Thread{
+    @Override
+    public void run(){
+        for(int i=1; i<=10; i++){
+            System.out.println("Launching missile #"+i + " Enemy: #2");
+        }
+    }
+   
+}
 public class threading {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Thread2 task = new Thread2();
+        task.start();
+        for(int i=1; i<=10; i++){
+            System.out.println("Launching missile #"+i + " Enemy: #1");
+        }
+      
+        
+      
     }
     
 }
