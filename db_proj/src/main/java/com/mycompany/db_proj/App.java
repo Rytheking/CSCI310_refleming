1307974d917d694985f62577bc3e122bf198924f
Ryan Fleming <ryanelliottfleming@gmail.com>
2019-11-14 23:11:31 -0700

    skeleton db, connection failing

--- db_proj/src/main/java/com/mycompany/db_proj/App.java
+++ db_proj/src/main/java/com/mycompany/db_proj/App.java
@@ -6,15 +6,14 @@
 package com.mycompany.db_proj;
 
 import java.sql.*;
-import java.util.Scanner;
-import java.util.regex.*;
+
 
 /**
  *
  * @author Ryan Fleming
  */
 public class App{
-    private static String url = "jdbc:sqlite:/Users/ryanfleming/Wendy's.db"; 
+    private static String url = "jdbc:sqlite:/Users/ryanfleming/illicit_substances.db"; 
     
     
     private Connection connect() {
@@ -28,9 +27,80 @@ public class App{
         return cnct;
     }
     
+    public static void createDatabase() {
+ 
+        try (Connection cnct = DriverManager.getConnection(url)) {
+            if (cnct != null) {
+                DatabaseMetaData meta = cnct.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+ 
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public static void createNewTable() {
+        // SQLite connection string
+        
+        
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS substances (\n"
+                + "    id integer PRIMARY KEY,\n"
+                + "    substance text NOT NULL,\n"
+                + "    schedule integer\n"
+                + ");";
+        
+        try (Connection conn = DriverManager.getConnection(url);
+                Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
     
+    public void insert(String substance, int schedule) {
+        String sql = "INSERT INTO illict_substances(name,capacity) VALUES(?,?)";
+ 
+        try (Connection conn = this.connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, substance);
+            pstmt.setDouble(2, schedule);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public void update(int id, String substance, int schedule) {
+        String sql = "UPDATE illict_substances SET name = ? , "
+                + "capacity = ? "
+                + "WHERE id = ?";
+ 
+        try (Connection cnct = this.connect();
+                PreparedStatement pstmt = cnct.prepareStatement(sql)) {
+ 
+            // set the corresponding param
+            pstmt.setString(1, substance);
+            pstmt.setDouble(2, schedule);
+            pstmt.setInt(3, id);
+            // update 
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public static void main(String[] args) {
+        new App().run();
+    }
 
     void run() {
-        connect();
+        createDatabase();
+        createNewTable();
+        insert("Raw Materials", 3000);
+        insert("Semifinished Goods", 4000);
+        insert("Finished Goods", 5000);
     }
 }
